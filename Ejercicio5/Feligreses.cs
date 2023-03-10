e50139310b19671648daf15371b893f4c21190a3
Abdiel Cuevas <61069624+Javier13g@users.noreply.github.com>
2023-03-09 20:39:32 -0400

    Exercise 5
    Desarrollar una aplicación para registrar la comiunión, ceniza y asitencia de los feligreses de la iglesia. La aplicación tendrá dos tablas:
    
    tblFeligreses
    
    id
    
    TipoDocumento
    
    Nombres
    
    Apellidos
    
    FechaNacimiento
    
    FechaIngreso
    
    Sexo
    
    Bautizado
    
    EstadoCivil
    
    Confirmacion
    
    CantidadComunion
    
    CantidadCeniza
    
    CantidadAsitencia
    
    
    
    tblActividad
    
    id
    
    TipoDocumento
    
    Documento
    
    TipoEvento
    
    FechaIngreso
    
    DigitadoPor
    
    Localidad
    
    Nota
    
    La localidad debe ser un parámetro en el archivo de configuración. Usar Stored Procedures. Hacer una transacción para lograr la integridad. Hacer un upsert en la tabla de feligreses para aumentar el contador correspondiente los enventos que le correspondan. Hacer log de todos los registros en archivo.

---
+++ Ejercicio5/Feligreses.cs
@@ -0,0 +1,16 @@
+public class Feligreses
+{
+    public string Nombre { get; set; }
+    public string Apellido { get; set; }
+    public int TipoDocumento { get; set; }
+    public string Documento { get; set; }
+    public string FechaNacimiento { get; set; }
+    public DateTime FechaIngreso { get; set; }
+    public string Sexo { get; set; }
+    public bool Bautizado { get; set; }
+    public string EstadoCivil { get; set; }
+    public bool Confirmacion { get; set; }
+    public int? CantidadComunion { get; set; }
+    public int? CantidadCeniza { get; set; }
+    public int? CantidadAsistencia { get; set; }
+}
---
+++ Ejercicio5/log.cs
@@ -0,0 +1,55 @@
+namespace Ejercicio5
+{
+    class log
+    {
+        public string mensajeLog { get; set; }
+        public Boolean mostrarConsola { get; set; }
+ 
+        //Constructor si se pasa el mensaje por parámetro en la creación de la clase
+        public log(string mensajeEnviar, Boolean mostrarConsola)
+        {
+            mensajeLog = mensajeEnviar;
+            if (mostrarConsola)
+                monstrarMensajeConsola();
+            escribirLineaFichero();
+        }
+        
+        //Constructor si se pasa el mensaje por setter tras la creación de la clase
+        public log()
+        {
+            if (mostrarConsola)
+                monstrarMensajeConsola();
+            escribirLineaFichero();
+        }
+ 
+        public void monstrarMensajeConsola()
+        {
+            //Quitar posibles saltos de línea del mensaje
+            mensajeLog = mensajeLog.Replace(Environment.NewLine, " | ");
+            mensajeLog = mensajeLog.Replace("\r\n", " | ").Replace("\n", " | ").Replace("\r", " | ");
+            Console.WriteLine(DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss") + " " + mensajeLog);
+        }
+ 
+        //Escribe el mensaje de la propiedad mensajeLog en un fichero en la carpeta del ejecutable
+        public void escribirLineaFichero()
+        {
+            try
+            {
+                FileStream fs = new FileStream(@AppDomain.CurrentDomain.BaseDirectory +
+                    "estado.log", FileMode.OpenOrCreate, FileAccess.Write);
+                StreamWriter m_streamWriter = new StreamWriter(fs);
+                m_streamWriter.BaseStream.Seek(0, SeekOrigin.End);
+                //Quitar posibles saltos de línea del mensaje
+                mensajeLog = mensajeLog.Replace(Environment.NewLine, " | ");
+                mensajeLog = mensajeLog.Replace("\r\n", " | ").Replace("\n", " | ").Replace("\r", " | ");                
+                m_streamWriter.WriteLine(DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss") + " " + mensajeLog);
+                m_streamWriter.Flush();
+                m_streamWriter.Close();
+            }
+            catch
+            {
+                //Silenciosa
+            }
+        }
+    }
+}
